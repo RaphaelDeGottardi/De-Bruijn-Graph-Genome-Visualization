08dfb7d1c4a038bfc14cf24a023874585de5a684
Raphael De Gottardi <raphael.degottardi@students.unibe.ch>
2024-04-17 13:18:30 +0200

    Added two functions to color unitg Nodes and compress the graph

--- debruijn.js
+++ debruijn.js
@@ -35,6 +35,7 @@ function makeGraph(k, reads) {
     return graph;
 }
 
+
 function toNetworkData(graph) {
     var nodesData = [];
     var edgesData = [];
@@ -54,10 +55,10 @@ function toNetworkData(graph) {
         });
         graph[key].forEach(function(item, index) {
             let e = key[0] + item;
-            if (e in edges) edges[e].count++;
+            if (e in edges) edges[e].count++; // increment count of the edge
             else edges[e] = {
-                from: keys.indexOf(key),
-                to: keys.indexOf(item),
+                from: keys.indexOf(key),      // or intialize the edge
+                to: keys.indexOf(item),   
                 count: 1
             };
         });
@@ -81,6 +82,60 @@ function toNetworkData(graph) {
     };
 }
 
+function colorNet(networkData){
+    var parentCounts = {}; // Object to count occurrences of each 'from key
+    var childCounts = {}; // Object to count occurrences of each 'to' key
+
+    // Count occurrences of each 'parent' key
+    networkData.edges.forEach(edge => {
+        const parentKey = edge.from;
+        if (parentCounts[parentKey]) {
+            parentCounts[parentKey]++;
+        } else {
+            parentCounts[parentKey] = 1;
+        }        
+        const childKey = edge.to;
+        if (childCounts[childKey]) {
+            childCounts[childKey]++;
+        } else {
+            childCounts[childKey] = 1;
+        }
+    });
+
+    // Filter edges that are part of unitig
+    var uNodes = networkData.nodes.filter(node => parentCounts[node.id] === 1 && childCounts[node.id] === 1);
+
+    uNodes.forEach(node => {
+        networkData.nodes.find(nnode => nnode.id === node.id).color = '#3300ff'
+    })
+    // merge edges
+    
+
+    return networkData
+}
+
+function compactNet(networkData){
+    compNodes = networkData.nodes.filter(node => node.color !== '#3300ff');
+    compNodeIds = new Set(compNodes.map(node => node.id));
+    compEdges = networkData.edges.filter(edge => compNodeIds.has(edge.from))
+    compEdges.forEach(edge => {
+            if(!compNodeIds.has(edge.to)){
+                degr = 0
+                while(!compNodeIds.has(edge.to)){
+                    degr++
+                    nextEdge = networkData.edges.find(nxtedge => nxtedge.from === edge.to)
+                    edge.to = nextEdge.to;
+                    edge.label = edge.label.substring(0,degr) + nextEdge.label
+            }}
+    })
+
+    return {
+        nodes: compNodes,
+        edges: compEdges
+    }
+}
+
+
 function visualizeNetwork(networkData) {
     // create a network
     var container = document.getElementById('mynetwork');
@@ -91,7 +146,11 @@ function visualizeNetwork(networkData) {
         edges: new vis.DataSet(networkData.edges),
     };
 
-    var options = {};
+    var options = {
+        edges: {
+            length: 200, // Adjust the length of the edges (in pixels)
+        },
+    };
     // initialize your network!
     var network = new vis.Network(container, data, options);
 }
